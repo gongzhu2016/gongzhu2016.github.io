1cf71908de3c9adae9710847d05551ea971a920a
HSZ <674959901@qq.com>
2016-02-05 15:41:31 -0800

    more photos

--- js/javascript.js
+++ js/javascript.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
       onLeave: function(index, nextIndex, direction) {
         if (nextIndex == 1) { $('div#header').fadeOut() }
         if (index == 1) { $('div#header').fadeIn() }
-        if (nextIndex == 6) { $('div#footer').fadeOut() }
-        if (index == 6) { $('div#footer').fadeIn() }
+        if (nextIndex == 11) { $('div#footer').fadeOut() }
+        if (index == 11) { $('div#footer').fadeIn() }
       },
-      easing:'easeOutBack'
+      easing:'easeInOutBack'
     });
   $('#fullpage').fullpage.setAllowScrolling(false);
 
